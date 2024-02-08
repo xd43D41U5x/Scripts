2c37b09f07ed4756a2ca185d7a912956209cfd49
xd43D41U5x <79152324+xd43D41U5x@users.noreply.github.com>
2024-02-07 22:21:28 -0500

    Update zloaderDGA.py

--- zloaderDGA.py
+++ zloaderDGA.py
@@ -3,11 +3,10 @@ import datetime
 def getMidToday():
     now = datetime.datetime.now(datetime.timezone.utc)
     midnight = now.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
-    timeStampToday = midnight.timestamp()
+    timeStampToday = int(midnight.timestamp())
     return timeStampToday
 
-
-time = int(getMidToday())
+time = getMidToday()
 
 results = []
 for x in range(0x20):
